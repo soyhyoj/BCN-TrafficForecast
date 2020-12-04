5212a07c2ed0eaddba3e40370458ec94b1453624
soyhyoj <soyhyoj@gmail.com>
2020-12-04 17:24:14 +0100

    db_to_df and db_t0_gdf added

--- notebooks/db.py
+++ notebooks/db.py
@@ -1,4 +1,11 @@
 import psycopg2
+import pandas as pd
+import geopandas as gpd
+from sqlalchemy import create_engine
+
+########################################################################
+# Functions for ETL #
+########################################################################
 
 # Define a function connect to the database 'bcn_traffic'
 # In case the connecting attempt fails, print the error message
@@ -60,26 +67,32 @@ def fill_table_with_data(query, filepath):
         conn.close ()
         #print('Connection closed')
 
+# Function to create connection using SQLALchemy
+def prepare_engine():
+    return create_engine('postgresql://bcn:bcn@postgis:5432/bcn_traffic')
 
-###########################################################3
-# Functions for EDA
+
+### End of ETL ###
+# To check the database using terminal:
+# 1. docker exec -it bcn-trafficforecast_postgis_1 bash
+# 2. psql postgresql://bcn:bcn@postgis:5432/bcn_traffic
+
+
+########################################################################
+# Functions for EDA #
+########################################################################
 
 # A function to retrieve data from database using queries
-# and save the result as a gdf (geodataframe)
-def db_to_df(table_name, column_names):
-    
+# and save the result as a df 
+def db_to_df(query, column_names):
     conn = connect_to_db()
-    
-    query = f"""
-            select * from {table_name}
-            limit 100
-            """
+
     try:
         cur = conn.cursor()
         cur.execute(query)
         
     except psycopg2.DatabaseError: # print error if fails
-        print ("Failed to create the dataframe")
+        print ("Failed to execute the query")
     
     tupples = cur.fetchall() # a list of tupples
     cur.close()
@@ -88,3 +101,18 @@ def db_to_df(table_name, column_names):
     # We just need to turn it into a pandas dataframe
     df = pd.DataFrame(tupples, columns=column_names)
     return df
+
+# Function to retrieve data from database using queries
+# and save the result as a gdf (geodataframe)
+def db_to_gdf(query, geometry):
+    conn = connect_to_db()
+    
+    try:
+        # Save the query result as a geodataframe
+        gdf = gpd.read_postgis(query, conn, geom_col=geometry)
+
+    except psycopg2.DatabaseError: # print error if fails
+        print ("Failed to execute the query")
+
+    conn.close () # Close connection
+    return gdf
